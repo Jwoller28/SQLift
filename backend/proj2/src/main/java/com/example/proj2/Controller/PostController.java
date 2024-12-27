aeffbcf3811ca88332d0e341eb0363906c9835ff
hasanthamdani <hasanthamdani01@gmail.com>
2024-12-26 20:56:59 -0500

    Streams, Producer, and Consumer are setup and working, just need to display results, restrict image size, and create tests before merging.

--- backend/proj2/src/main/java/com/example/proj2/Controller/PostController.java
+++ backend/proj2/src/main/java/com/example/proj2/Controller/PostController.java
@@ -1,9 +1,16 @@
 package com.example.proj2.Controller;
 
+import java.io.IOException;
 import java.util.Base64;
+import java.util.List;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,23 +27,42 @@ import com.example.proj2.Service.PostService;
 public class PostController {
     
     @Autowired
+    final static Logger logger = LoggerFactory.getLogger(PostController.class);
+
+    @Autowired
     private PostService postService;
 
-    @PostMapping("/posts")
-    public void sendMessage(@RequestParam("goal_id") long goalId,
+    @PostMapping("posts")
+    @CrossOrigin(origins = "http://localhost:3000")
+    // Turn the Request Param into Request Body with object that has these fields
+    public void sendPost(@RequestParam("goal_id") long goalId,
     @RequestParam("user_id") long userId,
     @RequestParam("message_text") String messageText,
-    @RequestPart("photo") String photo)
+    @RequestPart("photo") MultipartFile photo)
     {
+    logger.info("Arrives at Controller");
+
     Post post = new Post();
     post.setGoal_id(goalId);    
     post.setUser_id(userId);   
     post.setMessage_text(messageText);
-    byte[] decodedBytes = Base64.getDecoder().decode(photo);
-    System.out.println(photo);
-    post.setPhoto(decodedBytes);
-    postService.sendPost(post);
+
+        try {
+        byte[] photoBytes = photo.getBytes();
+        System.out.println(photoBytes);
+        post.setPhoto(photoBytes);
+    } catch (IOException e) {
+        e.printStackTrace();
     }
 
+    postService.sendPost(post);
+
+    }
+
+    @GetMapping("/posts")
+    public List<Post> getPosts()
+    {   
+       return postService.getPostList();
+    }
 }
 
--- frontend/revproj2/src/COMPONENTS/POSTFEED/PostFeedDumb.tsx
+++ frontend/revproj2/src/COMPONENTS/POSTFEED/PostFeedDumb.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { RefObject } from 'react'
 import { FormEventHandler } from 'react'
 import { MouseEventHandler } from 'react'
 
@@ -7,23 +7,23 @@ interface PostFeedProp {
     setUser : Function,
     setMessage : Function,
     onSubmit: FormEventHandler<HTMLFormElement>,
-    formRef : any
+    setFile : Function
 }
 function PostFeedDumb(prop : PostFeedProp) {
   return (
     <div>
-        <form ref = {prop.formRef} encType="multipart/form-data" id = "inputForm" name = "inputForm" onSubmit={prop.onSubmit}>
+        <form  encType="multipart/form-data" id = "inputForm" name = "inputForm" onSubmit={prop.onSubmit}>
             <label htmlFor="goal_id">Goal_Id</label> <br/>
             <input type="number" name="goal_id" onChange = {(e) => prop.setGoalId(e.target.value)} required></input> <br/>
 
-            <label htmlFor="sent_by">User ID</label> <br/>
-            <input type="number" name="sent_by" onChange = {(e) => prop.setUser(e.target.value)} required></input> <br/>
+            <label htmlFor="user_id">User ID</label> <br/>
+            <input type="number" name="user_id" onChange = {(e) => prop.setUser(e.target.value)} required></input> <br/>
 
             <label htmlFor="message_text">Message</label> <br/>
             <input type="text" name="message_text" onChange = {(e) => prop.setMessage(e.target.value)} required></input> <br/>
 
-            <label htmlFor="img_url">IMG</label> <br/>
-            <input type="file" name="img_url" accept='image/*' required></input> <br/>
+            <label htmlFor="photo">IMG</label> <br/>
+            <input type="file" required name="photo" accept='image/*' onChange = {(e) => prop.setFile(e.target.files ? e.target.files[0] : undefined)}></input> <br/>
 
             <button type="submit" id = "submitButton" name = "submitButton">Submit Post</button>
             </form>
