0caf5d0371aae537fe64509408b4bd333618e1e0
bretseelall <bretseelall@gmail.com>
2024-12-27 20:25:21 -0500

    Set User Goals Page Functionality/Authentication Token fix

--- backend/proj2/src/main/java/com/example/proj2/Configs/WebConfig.java
+++ backend/proj2/src/main/java/com/example/proj2/Configs/WebConfig.java
@@ -2,6 +2,9 @@ package com.example.proj2.Configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,8 +14,23 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedMethods("*")
+                .allowedHeaders("*").allowCredentials(true);
     }
+
+    // @Bean
+    // public CorsFilter corsFilter(){
+    //     CorsConfiguration configuration = new CorsConfiguration();
+    //     configuration.addAllowedOrigin("http://localhost:3000");
+    //     configuration.addAllowedMethod("*");
+    //     configuration.addAllowedHeader("*");
+    //     configuration.setAllowCredentials(true);
+
+    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    //     source.registerCorsConfiguration("/**", configuration);
+
+    //     return new CorsFilter(source);
+
+    // }
 }
--- frontend/revproj2/src/App.tsx
+++ frontend/revproj2/src/App.tsx
@@ -4,6 +4,8 @@ import UserManagement from './Components/UserLogin/UserManagement';
 import { AuthProvider } from './Components/UserContext/UserContext';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import UserRegistration from './Components/UserRegistration/UserRegistration';
+import SetUserGoals from './Components/SetUserGoals/SetUserGoals';
+import LoginLandingPage from './Components/UserLogin/LoginLandingPage';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<Navigate to="/login" replace />}/>
           <Route path="/login" element={<UserManagement/>}></Route>
           <Route path="/register" element={<UserRegistration/>}></Route>
+          <Route path="/goals" element={<SetUserGoals/>}></Route>
+          <Route path="/login/page" element={<LoginLandingPage/>}></Route>
         </Routes>
         
       </AuthProvider>
