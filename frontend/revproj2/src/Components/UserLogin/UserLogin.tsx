d8032a8adaceefdd267c59304cad979b10fc38bf
wadea1984 <wadea1984@gmail.com>
2025-01-03 09:37:08 -0500

    added logo

--- frontend/revproj2/src/Components/UserLogin/UserLogin.tsx
+++ frontend/revproj2/src/Components/UserLogin/UserLogin.tsx
@@ -1,7 +1,8 @@
 import { Navigate } from "react-router-dom"
 import { useNavigate } from 'react-router-dom';
 import { Link } from "react-router-dom"
-//import logo from '/Logo/logo-transparent-png.png'; 
+
+
 
 
 // Creating the typing for the props that are being brought into UserLogin from UserManagement
@@ -22,7 +23,7 @@ function UserLogin({username, setUsername, password, setPassword, handleSubmit, 
 
   return (
     <div>
-       <img src={"C:/Users/wadea/revProj2/frontend/revproj2/src/Components/Logo/logo-transparent-png.png"} alt="logo" />
+       
     <form onSubmit={handleSubmit}>
         <label>Username:
             <input type='text' placeholder="Enter Username" value={username} onChange={(e:any) => setUsername(e.target.value)} required/>
