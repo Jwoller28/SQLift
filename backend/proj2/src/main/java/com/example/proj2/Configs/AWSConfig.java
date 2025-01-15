0e5295068eeabecc8434d03a0af9fdbd4245a384
bretseelall <bretseelall@gmail.com>
2025-01-14 23:37:31 -0500

    Updated Backend Files

--- backend/proj2/src/main/java/com/example/proj2/Configs/AWSConfig.java
+++ backend/proj2/src/main/java/com/example/proj2/Configs/AWSConfig.java
@@ -1,3 +1,45 @@
+// package com.example.proj2.Configs;
+
+// import com.amazonaws.auth.AWSCredentials;
+// import com.amazonaws.auth.AWSStaticCredentialsProvider;
+// import com.amazonaws.auth.BasicAWSCredentials;
+// import com.amazonaws.regions.Regions;
+// import com.amazonaws.services.s3.AmazonS3;
+// import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+// import java.net.URI;
+// import java.net.URISyntaxException;
+// import com.example.proj2.Configs.MyConfig;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+// import org.springframework.boot.context.properties.ConfigurationProperties;
+// import org.springframework.cloud.vault.config.Secrets;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.vault.annotation.VaultPropertySource;
+// import org.springframework.vault.authentication.TokenAuthentication;
+// import org.springframework.vault.client.VaultEndpoint;
+// import org.springframework.vault.core.VaultTemplate;
+// import org.springframework.vault.support.VaultResponseSupport;
+
+
+// @Configuration
+// public class AWSConfig {
+
+// 	@Bean
+// 	public AmazonS3 s3Client() {
+
+// 		AWSCredentials credentials = new BasicAWSCredentials(config.getAccessKey(), config.getAccessSecret());
+// 		return AmazonS3ClientBuilder.standard()
+// 			.withCredentials(new AWSStaticCredentialsProvider(credentials))
+// 			.withRegion(config.getRegion())
+// 			.build();
+
+// 	}
+// }
+
 package com.example.proj2.Configs;
 
 import com.amazonaws.auth.AWSCredentials;
@@ -11,18 +53,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/*
 @Configuration
 public class AWSConfig {
 
-	@Value("${cloud.aws.credentials.accessKey}")
+	@Value("${aws.access.keyId}")
 	private String accessKey;
 
-	@Value("${cloud.aws.credentials.secretKey}")
+	@Value("${aws.access.secret}")
 	private String accessSecret;
 
-	@Value("${cloud.aws.region.static}")
-	private String region;
+	private String region = "us-east-2";
 
 	@Bean
 	public AmazonS3 s3Client() {
@@ -35,4 +75,4 @@ public class AWSConfig {
 
 	}
 }
-*/
+
--- backend/proj2/src/main/java/com/example/proj2/Proj2Application.java
+++ backend/proj2/src/main/java/com/example/proj2/Proj2Application.java
@@ -1,18 +1,23 @@
 package com.example.proj2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
-public class Proj2Application {
+import com.amazonaws.services.dynamodbv2.xspec.M;
 
+@SpringBootApplication
+public class Proj2Application{
 	public static void main(String[] args) {
 		SpringApplication.run(Proj2Application.class, args);
 	}
-
 	// @Bean
 	// public WebMvcConfigurer corsConfigurer() {
 	// 	return new WebMvcConfigurer() {
